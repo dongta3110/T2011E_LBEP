ed093ce1b6cb953a403d9ae48fc7bda1bc4a5bf9
dongta3110 <75368551+dongta3110@users.noreply.github.com>
2020-12-09 15:34:50 +0700

    Add files via upload

---
+++ bvnmang1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("Nhap n=");
+	scanf("%d",&n);
+	int ary[n];
+	for(int i=0; i<n; i++) {
+	printf("\n Enter value: %d :", i+1);
+	scanf("%d", &ary[i]);
+	}
+	
+	int slcc = 0;
+	for(int i=0;i<n;i++) {
+		if(ary[i] %2 != 0) {
+			slcc = ary[i];
+		}
+	}
+	if(slcc==0) {
+		printf("mang ko co so le");
+	}
+	else printf("so le cuoi cung la: %d",slcc);
+}
